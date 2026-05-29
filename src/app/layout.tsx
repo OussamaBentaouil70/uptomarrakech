daa9d352889737fb148516ba00918feb1805994e
OussamaBentaouil70 <ousamabentaouil1908@gmail.com>
2026-05-29 09:58:14 +0100

    added tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export const metadata: Metadata = {
   title: "MyMarrakechTrip",
   description: "Votre expérience exclusive à Marrakech commence ici.",
   metadataBase: new URL("https://mymarrakechtrip.com"),
+  verification: {
+    google: "scTRI76xFbrUt4evt8QnqD29xjohROur4l7N61gK-ZU",
+  },
   icons: {
     icon: "/assets/MyMarrakechTrip_logo.ico",
   },
@@ -46,7 +49,26 @@ export default function RootLayout({
       className={`${sans.variable} ${display.variable} h-full antialiased`}
       suppressHydrationWarning
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MMPTLXXR');`,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MMPTLXXR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Providers>
           {/* PublicComponents currently renders Header, Footer, and FAB. 
               To fix the order, we need the Header here, then children, then Footer. */}
